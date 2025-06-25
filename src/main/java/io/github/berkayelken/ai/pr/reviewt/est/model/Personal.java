d492d340d6f21971ba5f239dadb7555025e984ad
Berkay Yelken <berkayelken@gmail.com>
2025-06-25 11:24:38 +0300

    human and personel created

---
+++ src/main/java/io/github/berkayelken/ai/pr/reviewt/est/model/Personal.java
@@ -0,0 +1,15 @@
+package io.github.berkayelken.ai.pr.reviewt.est.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Personal {
+	private String id;
+	private String name_surname;
+	private int salary;
+	private String title;
+}
