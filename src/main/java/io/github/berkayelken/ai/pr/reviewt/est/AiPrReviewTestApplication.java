d8f0fac9b62635b186f6bf83aa050c674bd13e7d
Berkay Yelken <berkayelken@gmail.com>
2025-06-25 11:13:31 +0300

    init

---
+++ src/main/java/io/github/berkayelken/ai/pr/reviewt/est/AiPrReviewTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.ai.pr.reviewt.est;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPrReviewTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPrReviewTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/berkayelken/ai/pr/reviewt/est/AiPrReviewTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.berkayelken.ai.pr.reviewt.est;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPrReviewTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
